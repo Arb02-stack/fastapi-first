653fe7c9eb86b5f91e56c6c3584caf4d9d9c2099
arix <arix0001@gmail.com>
2026-08-25 23:14:12 -0300

    Concelamento de pedido.

--- src/app/order_routes.py
+++ src/app/order_routes.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from schemas import PedidoSchema
 from sqlalchemy.orm import Session
-from dependencies import pegar_sessao
-from models import Pedido
+from dependencies import pegar_sessao, verificar_token
+from models import Pedido, Usuario
 
-order_router = APIRouter(prefix='/pedidos', tags=['orders'])
+order_router = APIRouter(
+        prefix='/pedidos',
+        tags=['orders'],
+        dependencies=[Depends(verificar_token)]
+)
  
 @order_router.get("/")
 async def home_orders():
@@ -23,3 +27,25 @@ async def criar_pedido(pedido_schema: PedidoSchema, session: Session=Depends(peg
     return {
             "mensagem": f"Pedido criado com sucesso.\nId do produto: [{novo_pedido.id}]"
            }
+
+# cancelar pedido
+@order_router.post("/Pedido/cancelar/{id_pedido}")
+async def cancelar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao)):
+    pedido = session.query(Pedido).filter(Pedido.id == id_pedido).first()
+
+    if not pedido:
+        raise HTTPException(
+                    status_code=400,
+                    detail="Pedido não econtrado."
+                )
+    
+    pedido.status = "CANCELADO"
+
+    session.commit()
+    return {
+        "mensagem": f"Pedido [{id_pedido}] concelado com sucesso!",
+        "pedido"  : pedido
+    }
+
+
+
